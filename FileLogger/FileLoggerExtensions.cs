58eeae4070de43b1c4f418f0e1f37d652e7ea8ef
Kingloo <k1ngloo@hotmail.com>
2021-12-27 00:03:59 +0100

    Remove old comment

--- FileLogger/FileLoggerExtensions.cs
+++ FileLogger/FileLoggerExtensions.cs
@@ -13,9 +13,6 @@ namespace FileLogger
 
 			loggingBuilder.Services.AddSingleton<ILoggerProvider, FileLoggerProvider>();
 
-			// loggingBuilder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, FileLoggerProvider>());
-			// https://docs.microsoft.com/en-gb/dotnet/api/microsoft.extensions.dependencyinjection.extensions.servicecollectiondescriptorextensions.tryaddenumerable
-
 			loggingBuilder.Services.Configure(configure);
 
 			return loggingBuilder;
