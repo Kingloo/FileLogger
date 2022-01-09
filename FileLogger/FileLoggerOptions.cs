48b3078399e2b7fb9558a5b20274a5496faf1e56
Kingloo <k1ngloo@hotmail.com>
2022-01-09 18:53:40 +0100

    Change default timestamp format, increase drain count

--- FileLogger/FileLoggerOptions.cs
+++ FileLogger/FileLoggerOptions.cs
@@ -6,14 +6,14 @@ namespace FileLogger
 {
 	public class FileLoggerOptions
 	{
-		public const string DefaultTimestampFormat = "dd/MM/yyyy HH:mm:ss zzz";
+		public const string DefaultTimestampFormat = "yyyy-MM-ddTHH:mm:ss.fffzzz";
 
 		public string Path { get; set; } = string.Empty;
 		public LogLevel LogLevel { get; set; } = LogLevel.Information;
 		public string TimestampFormat { get; set; } = DefaultTimestampFormat;
 		public bool UseUtcTimestamp { get; set; } = false;
 		public int DrainIntervalMs { get; set; } = 1000;
-		public int DrainCount { get; set; } = 10;
+		public int DrainCount { get; set; } = 100;
 
 		public FileLoggerOptions() { }
 
