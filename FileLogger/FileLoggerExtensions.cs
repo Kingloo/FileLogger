9f05edd26ee3a45864c62ed05e3cbb1879de4337
Kingloo <k1ngloo@hotmail.com>
2022-01-09 18:39:33 +0100

    Remove call to LoggerProviderOptions.RegisterProviderOptions
    Not needed if you configure the options object as done in AspNetCoreSample

--- FileLogger/FileLoggerExtensions.cs
+++ FileLogger/FileLoggerExtensions.cs
@@ -1,7 +1,6 @@
 using System;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Configuration;
 
 namespace FileLogger
 {
@@ -9,8 +8,6 @@ namespace FileLogger
 	{
 		public static ILoggingBuilder AddFileLogger(this ILoggingBuilder loggingBuilder, Action<FileLoggerOptions> configure)
 		{
-			LoggerProviderOptions.RegisterProviderOptions<FileLoggerOptions, FileLoggerProvider>(loggingBuilder.Services);
-
 			loggingBuilder.Services.AddSingleton<ILoggerProvider, FileLoggerProvider>();
 
 			loggingBuilder.Services.Configure(configure);
