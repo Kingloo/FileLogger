fa08e677c01f19402fac4b3a7d99f114029cc6e6
Kingloo <k1ngloo@hotmail.com>
2022-03-13 16:59:57 +0100

    Ignore warning
    I want to call it that.

--- FileLogger/IFileLoggerSink.cs
+++ FileLogger/IFileLoggerSink.cs
@@ -7,7 +7,9 @@ namespace FileLogger
 	public interface IFileLoggerSink : IAsyncDisposable
 	{
 		void Start(FileLoggerOptions options);
+#pragma warning disable CA1716
 		void Stop();
+#pragma warning restore CA1716
 		void Pour(string message);
 		ValueTask DrainAsync();
 		ValueTask DrainAsync(CancellationToken cancellationToken);
