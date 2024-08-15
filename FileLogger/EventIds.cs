aa00c36709e95a0d61b8567a92dabd74df5a6bb0
Kingloo <k1ngloo@hotmail.com>
2024-08-15 20:00:06 +0200

    Remove unused method

--- FileLogger/EventIds.cs
+++ FileLogger/EventIds.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-
 namespace FileLogger
 {
 	public static class EventIds
@@ -8,24 +5,5 @@ namespace FileLogger
 		public const int Disposed = 0;
 		public const int Timer = 1;
 		public const int OptionsUpdated = 2;
-
-		public static bool ShouldLogFileLoggerEventId(IList<int> eventIds, int eventId)
-		{
-			ArgumentNullException.ThrowIfNull(eventIds);
-			ArgumentOutOfRangeException.ThrowIfLessThan<int>(eventId, 0);
-
-			if (eventIds.Count == 0)
-			{
-				return false;
-			}
-
-			if (eventIds.Count == 1
-				&& eventIds[0] == -1)
-			{
-				return true;
-			}
-
-			return eventIds.Contains(eventId);
-		}
 	}
 }
